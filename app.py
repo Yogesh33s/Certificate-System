3931deed3c47e2a81caac60d7ea46d8f3b361429
Yogesh <150877199+Yogesh33s@users.noreply.github.com>
2025-11-19 22:35:09 +0530

    Configure app to run on specified port from env

--- app.py
+++ app.py
@@ -243,4 +243,7 @@ def download_pdf(student_id, cert_type):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
